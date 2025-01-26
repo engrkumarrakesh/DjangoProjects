ef6283d2639c6ee8fce8cbaa1f455c01bda79125
fornegolabs <fornegolabs@gmail.com>
2025-01-26 22:10:54 +0530

    view addded

--- templates_03/views.py
+++ templates_03/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import ContactForm
+from datetime import datetime
 
 
 # Create your views here.
 def templates_03(request):
+    dt=datetime.now()
     gender="male"
-    name={'name':'Kumar Rakesh','age':32,'gender':gender}
+    name={'name':'Kumar Rakesh','age':32,'gender':gender,'dt':dt}
     # return render(request, 'index.html',context=name)
     # return render(request, 'index.html',{'name':'Kumar Rakesh'})
-    return render(request, 'index.html',name)
+    students={'name':['Rakesh Kumar','Sachin Tendulkar','Rahul Dravid']}
+    return render(request, 'index.html',students)
 
 def contact_form(request):
     if request.method == 'POST':
